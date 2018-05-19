26bea9a0a36c20731dee9d9f3fb1892ae620f614
celia <tcc_2007@126.com>
2018-05-19 17:12:14 +0800

    循环出电影

--- page/index/index.js
+++ page/index/index.js
@@ -14,12 +14,18 @@ Page({
     subtitle: '加载中...',
   },
 
-  loadMore () {
-    if (!this.data.hasMore) return
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function () {
+    this.loadMovies()
+  },
 
-    wx.showLoading({
-      title: '拼命加载中...',
-    })
+  loadMovies () {
+
+    // wx.showLoading({
+    //   title: '拼命加载中...',
+    // })
 
     // this.setData({subtitle: '加载中...'})
 
@@ -37,37 +43,41 @@ Page({
     //     console.error(e)
     //     wx.hideLoading()
     //   })
-
-    return app.douban.find('top250', 1, 8)
+    this.setData({ loading: true})
+    return app.douban.find('top250', this.data.page++, this.data.size)
     .then(d => {
-      console.log(d)
-    })
+      const data = d.subjects
+      const movies = this.data.movies
 
-    console.log(tasks)
+      for (let i = 0; i < data.length; i+=2) {
+        movies.push([data[i], data[i+1] ? data[i+1] : null])
+      }
 
-    Promise.all(tasks).then(boards => {
-      this.setData({ boards: boards, loading: false })
-      wx.hideLoading()
+      this.setData({ movies, loading: false}) 
     })
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (params) {
-    this.data.title = params.title || this.data.title
+  scrollHandler () {
+    const { page } = this.data
+    this.setData({
+      page:page+1
+    })
+    this.loadMovies()
+  },
 
-    // 类型： in_theaters  coming_soon  us_box
-    this.data.type = params.type || this.data.type
-
-    this.loadMore()
+  gotoDetail (e) {
+    console.log(e)
+    const { id } = e.currentTarget.dataset.id
+    wx.navigateTo({
+      url: '',
+    })
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-    wx.setNavigationBarTitle({ title: this.data.title + ' « 电影 « 豆瓣' })
+    
   },
 
   /**
