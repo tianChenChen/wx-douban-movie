8142726c49dfcde47f7f2dcc6e7c0003fa3096a3
celia <tcc_2007@126.com>
2018-05-23 18:08:18 +0800

    用户名

--- page/user/user.js
+++ page/user/user.js
@@ -5,14 +5,19 @@ Page({
    * 页面的初始数据
    */
   data: {
-  
+    avatarUrl: '',
+    nickName: '未知'
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+    wx.getUserInfo({
+      success(res) {
+        console.log(res)
+      }
+    })
   },
 
   /**
